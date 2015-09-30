db4e33f4eaf63f8403a1ce109597cd3819e2b752
Shun ISHIHARA <s3.ishihara@gmail.com>
2015-09-30 18:32:26 +0900

    fix knock 01: 問題文を読み間違えていた

--- chapter1/knock01.py
+++ chapter1/knock01.py
@@ -5,10 +5,10 @@
 # 「パタトクカシーー」という文字列の1,3,5,7文字目を取り出して連結した文字列を得よ.
 
 text   = u"パタトタカシーー"  # u"文字列" で unicode 文字列になる
-answer = u"タタシー"
+answer = u"パトカー"
 
-result = text[1::2]
+result = text[::2]
 
 assert result == answer
 print("Result: '" + result + "'")
-# => "Result: 'タタシー'"
+# => "Result: 'パトカー'"
